488a3e585bd06a11cb96f0f16cc561ab9b7a8548
TypicallyTristan <wintsch.tristan@gmail.com>
2025-03-26 00:38:45 -0400

    added result and fun messages after

--- LOL_rolepicker.py
+++ LOL_rolepicker.py
@@ -4,8 +4,7 @@ import random
 
 random_int = random.randint(0,4)
 
-
-Roles = ["Top", "Jungle", "Mid", "Bottom", "Support"]
+roles = ["Top", "Jungle", "Mid", "Bot", "Support"]
 
 print("Hey there!")
 
@@ -37,3 +36,31 @@ while True:
 
 time.sleep(1)
 print("Great! You already know the roles so lets just get right into it.")
+
+time.sleep(1)
+print(".")
+time.sleep(1)
+print("..")
+time.sleep(1)
+print("...")
+
+
+result = roles[random_int]
+
+time.sleep(1)
+print("You got", result)
+if result == ("Top"):
+    time.sleep(1)
+    print("Have fun on your island")
+elif result == ("Jungle"):
+    time.sleep(1)
+    print("Enjoy afk farming")
+elif result == ("Mid"):
+    time.sleep(1)
+    print("Run it down for me :)")
+elif result == ("Bot"):
+    time.sleep(1)
+    print("sensing a 0/3 in your future")
+elif result == ("Support"):
+    time.sleep(1)
+    print("my support can't hit a single hook")
